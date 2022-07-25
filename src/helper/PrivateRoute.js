dac8ca8ee8584070252640b256d6ce23d496aa0e
aymenkasP <ikoaymen056@gmail.com>
2022-07-25 19:19:57 +0100

    fix PrivateRoute

--- src/helper/PrivateRoute.js
+++ src/helper/PrivateRoute.js
@@ -2,9 +2,8 @@ import { Navigate } from "react-router-dom";
 import { supabase } from "../supabase/supabaseClient";
 
 export default function PrivateRoute({ children }) {
-    const session = true
+    const session = supabase.auth.session();
 
-    /* supabase.auth.session(); */
 
     return session ? children : <Navigate to="/login" />;
   }
